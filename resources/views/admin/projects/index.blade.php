5bc933d4eea46b17793332a648fe50067087a2a0
RosaD97 <r960595@gmail.com>
2023-05-17 16:08:07 +0200

    feat: admin/listProjects

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h2>
+        Lista projects
+    </h2>
+
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Title</th>
+            <th scope="col">Text</th>
+            <th scope="col">Start date</th>
+            <th scope="col">Slug</th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ($projects as $project)
+            <tr>
+                <th scope="row">1</th>
+                <td>{{ $project->title}}</td>
+                <td>{{ $project->text}}</td>
+                <td>{{ $project->start_date }}</td>
+                <td>{{ $project->slug}}</td>
+                <td>
+                    <a class="btn btn-primary" href="http://">Show</a>
+                    <a class="btn btn-primary" href="http://">Edit</a>
+                    <a class="btn btn-primary" href="http://">Show</a>
+                </td>
+
+              </tr>
+                
+            @endforeach
+        </tbody>
+      </table>
+
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,18 +15,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+ Route::get('/', function () {
+     return view('welcome');
+ });
 
-Route::get('/dashboard', function () {
-    return view('admin.dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/', [ProjectController::class, 'index'])->name('dashboard');
+    Route::resource('projects', ProjectController::class);
+
+
+    
+    // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
